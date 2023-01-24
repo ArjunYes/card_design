7b120af7f2b78a6081bc0c8d8112078b2931be05
ArjunYes <codingnerd1998@gmail.com>
2023-01-24 22:42:23 +0000

    css card project

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log("Loaded js file")
+
+let cards = document.querySelectorAll('.card')
+cards.forEach((card)=>{
+    card.addEventListener('click',()=>{
+        console.log("Click")
+        removeClass();
+        card.classList.add('active')
+    })
+})
+
+
+function removeClass(){
+    cards.forEach((card)=>{
+       card.classList.remove('active')
+    })  
+}
+console.log(cards)
